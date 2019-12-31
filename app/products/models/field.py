7e406798d42fcc34428d9412cf1a3282842e88b6
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2019-12-31 20:44:43 +0330

    Refactor field model

--- app/products/models/field.py
+++ app/products/models/field.py
@@ -2,18 +2,25 @@ from django.db import models
 
 
 class Field(models.Model):
-    name = models.CharField(max_length=64,
-                            verbose_name='نام')
-    category = models.ForeignKey('products.Category',
-                                 on_delete=models.CASCADE,
-                                 verbose_name='دسته')
+    name = models.CharField(
+        max_length=64,
+        verbose_name='نام'
+    )
+
+    category = models.ForeignKey(
+        'products.Category',
+        on_delete=models.CASCADE,
+        verbose_name='دسته'
+    )
 
     class Meta:
         verbose_name = 'فیلد'
         verbose_name_plural = 'فیلدها'
         constraints = [
-            models.UniqueConstraint(fields=['name', 'category'],
-                                    name='unique_field')
+            models.UniqueConstraint(
+                fields=['name', 'category'],
+                name='unique_field'
+            )
         ]
 
     def __str__(self):
