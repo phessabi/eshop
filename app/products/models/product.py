a14ca04942c6c966deba945899bcbbe63b6a2ac7
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2019-12-31 20:43:53 +0330

    Refactor product model

--- app/products/models/product.py
+++ app/products/models/product.py
@@ -2,24 +2,42 @@ from django.db import models
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=64,
-                             verbose_name='عنوان')
-    category = models.ForeignKey('products.Category',
-                                 on_delete=models.CASCADE,
-                                 verbose_name='دسته')
-    image = models.ImageField(upload_to='resources/images/products',
-                              null=True,
-                              blank=True,
-                              verbose_name='تصویر')
-    price = models.CharField(max_length=50,
-                             verbose_name='قیمت')
-    express = models.BooleanField(default=False,
-                                  verbose_name='فوری')
-    vendor = models.ForeignKey('accounts.Vendor',
-                               on_delete=models.CASCADE,
-                               null=True,
-                               blank=True,
-                               verbose_name='فروشنده')
+
+    title = models.CharField(
+        max_length=64,
+        verbose_name='عنوان'
+    )
+
+    category = models.ForeignKey(
+        'products.Category',
+        on_delete=models.CASCADE,
+        verbose_name='دسته'
+    )
+
+    image = models.ImageField(
+        upload_to='resources/images/products',
+        null=True,
+        blank=True,
+        verbose_name='تصویر'
+    )
+
+    price = models.CharField(
+        max_length=50,
+        verbose_name='قیمت'
+    )
+
+    express = models.BooleanField(
+        default=False,
+        verbose_name='فوری'
+    )
+
+    vendor = models.ForeignKey(
+        'accounts.Vendor',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name='فروشنده'
+    )
 
     class Meta:
         verbose_name = 'محصول'
