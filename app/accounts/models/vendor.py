b6bf1f6264e73b828ef4472c316331d012f4d17c
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2019-12-31 21:01:21 +0330

    Refactor vendor model

--- app/accounts/models/vendor.py
+++ app/accounts/models/vendor.py
@@ -2,18 +2,29 @@ from django.db import models
 
 
 class Vendor(models.Model):
-    user = models.OneToOneField('auth.User',
-                                on_delete=models.CASCADE,
-                                verbose_name='کاربر')
-    name = models.CharField(max_length=100,
-                            verbose_name='نام')
-    logo = models.ImageField(upload_to='resources/images/vendors',
-                             null=True,
-                             blank=True,
-                             verbose_name='لوگو')
-    credit = models.CharField(max_length=50,
-                              default='0',
-                              verbose_name='اعتبار')
+    user = models.OneToOneField(
+        'auth.User',
+        on_delete=models.CASCADE,
+        verbose_name='کاربر'
+    )
+
+    name = models.CharField(
+        max_length=100,
+        verbose_name='نام'
+    )
+
+    logo = models.ImageField(
+        upload_to='resources/images/vendors',
+        null=True,
+        blank=True,
+        verbose_name='لوگو'
+    )
+
+    credit = models.CharField(
+        max_length=50,
+        default='0',
+        verbose_name='اعتبار'
+    )
 
     class Meta:
         verbose_name = 'فروشنده'
