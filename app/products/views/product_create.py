61a91c7b88ea200ca6f8b1fd183d9ef3d6fe10b0
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-01-01 02:10:23 +0330

    Add list vendor products api

--- app/products/views/product_create.py
+++ app/products/views/product_create.py
@@ -1,10 +1,16 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
 
 from _helpers.permissions import IsVendor
+from products.models import Product
 from products.serializers import ProductSerializer
 
 
-class ProductCreateView(CreateAPIView):
+class ProductCreateView(GenericViewSet, CreateModelMixin, ListModelMixin):
     permission_classes = (IsAuthenticated, IsVendor)
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        vendor = self.request.user.vendor
+        return Product.objects.filter(vendor=vendor)
