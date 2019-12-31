2d9ae6ce0d543a5ec133ae78d9194b638cb17ed2
Hossein Keshavarz <hoseinkeshavarz1997@gmail.com>
2020-01-01 00:51:34 +0330

    implement search

--- app/app/settings.py
+++ app/app/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
