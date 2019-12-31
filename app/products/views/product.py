7b3c55c4d168ad7b709677190ff99fb9919b8ba9
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2019-12-31 23:46:21 +0330

    Refactor product list view

--- app/products/views/product.py
+++ app/products/views/product.py
@@ -1,23 +1,12 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.mixins import ListModelMixin
 from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
-from products.models import Product, Specification
-from products.serializers.product import ProductSerializer
-from products.serializers.specification import SpecificationSerializer
+from products.models import Product
+from products.serializers import ProductSerializer
 
 
-class ListRetrieveProductViewSet(GenericViewSet, ListAPIView, RetrieveAPIView):
+class ListRetrieveProductViewSet(GenericViewSet, ListModelMixin):
     permission_classes = (AllowAny,)
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = self.get_object()
-        product_serializer = self.get_serializer(instance)
-        spec = Specification.objects.get(product=instance.id)
-        spec_serializer = SpecificationSerializer(spec)
-        data = {**product_serializer.data, **spec_serializer.data}
-        return Response(data)
-
