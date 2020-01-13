0ce3e7bd83774c9caeb36d2895e2b9c0befb5605
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-01-14 01:54:50 +0330

    Add specifications to product serializer readonly fields

--- app/products/serializers/product.py
+++ app/products/serializers/product.py
@@ -9,7 +9,7 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ('id', 'title', 'category', 'price', 'express', 'archived', 'specifications')
-        read_only_fields = ('id', 'deleted')
+        read_only_fields = ('id', 'deleted', 'specifications')
 
     def create(self, validated_data):
         validated_data['vendor_id'] = self.context.get('vendor_id')
