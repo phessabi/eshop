65a15ee18d8c7f5cabee0eda16e7315bbc1132eb
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-01-14 01:37:41 +0330

    Add specifications to product serializer

--- app/products/serializers/product.py
+++ app/products/serializers/product.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 from products.models import Product
+from products.serializers.specification import SpecificationSerializer
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    specifications = SpecificationSerializer(many=True)
+
     class Meta:
         model = Product
-        fields = ('id', 'title', 'category', 'price', 'express', 'archived')
+        fields = ('id', 'title', 'category', 'price', 'express', 'archived', 'specifications')
         read_only_fields = ('id', 'deleted')
 
     def create(self, validated_data):
