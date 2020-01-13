9a40a266876d04aac404a35576c33c0fb3f89fc2
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-01-14 01:54:22 +0330

    Add field_name to specification serializer as a readonly field

--- app/products/serializers/specification.py
+++ app/products/serializers/specification.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
+
 from products.models import Specification
 
 
 class SpecificationSerializer(serializers.ModelSerializer):
+    field_name = serializers.SerializerMethodField()
+
     class Meta:
         model = Specification
-        fields = ('field', 'value')
+        fields = ('id', 'product', 'field', 'field_name', 'value')
+        read_only_field = ('id', 'field_name')
+
+    @staticmethod
+    def get_field_name(instance: Specification):
+        return instance.field.name
