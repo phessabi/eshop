47ffb3d93809d5acaef69b3443538a031a49d9aa
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-01-14 02:15:59 +0330

    Refactor public category view

--- app/products/views/public_views/category.py
+++ app/products/views/public_views/category.py
@@ -1,12 +1,11 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny
-from rest_framework.viewsets import GenericViewSet
 
 from products.models import Category
 from products.serializers import CategorySerializer
 
 
-class ListRetrieveFieldViewSet(GenericViewSet, ListAPIView, RetrieveAPIView):
+class ListCategoryView(ListAPIView):
     permission_classes = (AllowAny,)
-    queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    queryset = Category.objects.all()
