47dcd252e50b8765cd5c5d282f4079331640ebe5
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-01-14 01:37:25 +0330

    Add fields to category serializer

--- app/products/serializers/category.py
+++ app/products/serializers/category.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from products.models import Category
+from products.serializers.field import FieldSerializer
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    fields = FieldSerializer(many=True)
+
     class Meta:
         model = Category
-        fields = ('id', 'name', 'level', 'parent_category')
+        fields = ('id', 'name', 'level', 'parent_category', 'fields')
