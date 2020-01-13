e118d294a00543f4f6ef919e41c89b9087d80f37
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-01-14 02:39:00 +0330

    Refactor products app files

--- app/products/views/__init__.py
+++ app/products/views/__init__.py
@@ -1,2 +1,6 @@
 from .public_views.product import ListProductViewSet
+from .public_views.category import ListCategoryView
 from .vendor_views.product import VendorProductViewSet
+from .vendor_views.specification import VendorSpecificationViewSet
+from .admin_views.category import AdminCategoryViewSet
+from .admin_views.field import AdminFieldViewSet
