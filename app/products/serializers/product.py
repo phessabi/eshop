434d0cd259857d64e280c828cd10fc07b95f3963
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-01-14 01:23:09 +0330

    Fix bug on adding vendor id to serializer context

--- app/products/serializers/product.py
+++ app/products/serializers/product.py
@@ -5,9 +5,9 @@ from products.models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('id', 'title', 'category', 'price', 'express', 'deleted')
+        fields = ('id', 'title', 'category', 'price', 'express', 'archived')
         read_only_fields = ('id', 'deleted')
 
     def create(self, validated_data):
-        validated_data['vendor'] = self.context.get('vendor')
+        validated_data['vendor_id'] = self.context.get('vendor_id')
         return super().create(validated_data)
--- app/products/views/product_create.py
+++ app/products/views/product_create.py
@@ -1,5 +1,7 @@
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from _helpers.permissions import IsVendor
 from products.models import Product
@@ -7,12 +9,13 @@ from products.serializers import ProductSerializer
 
 
 class VendorProductViewSet(ModelViewSet):
+    authentication_classes = (JWTAuthentication, TokenAuthentication)
     permission_classes = (IsAuthenticated, IsVendor)
     serializer_class = ProductSerializer
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
-        context['vendor'] = self.request.user.vendor.id
+        context['vendor_id'] = self.request.user.vendor.id
         return context
 
     def get_queryset(self):
