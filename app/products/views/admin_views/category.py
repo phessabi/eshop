3d8f653d95d28aac5fc263afe1ab6cd615fea6f1
Hossein Keshavarz <hoseinkeshavarz1997@gmail.com>
2020-01-15 11:56:22 +0330

    fix admin category view bug

--- app/products/views/admin_views/category.py
+++ app/products/views/admin_views/category.py
@@ -1,9 +1,11 @@
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.viewsets import ModelViewSet
 
+from products.models import Category
 from products.serializers import CategorySerializer
 
 
 class AdminCategoryViewSet(ModelViewSet):
     permission_classes = (IsAuthenticated, IsAdminUser)
     serializer_class = CategorySerializer
+    queryset = Category.objects.all()
