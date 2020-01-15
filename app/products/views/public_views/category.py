0e16b8f1126af7d5a58ed0e9e81fbe088c10b827
Hossein Keshavarz <hoseinkeshavarz1997@gmail.com>
2020-01-15 12:02:47 +0330

    make category view inherit from viewset

--- app/products/views/public_views/category.py
+++ app/products/views/public_views/category.py
@@ -1,11 +1,12 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.mixins import ListModelMixin
 from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import GenericViewSet
 
 from products.models import Category
 from products.serializers import CategorySerializer
 
 
-class ListCategoryView(ListAPIView):
+class ListCategoryView(GenericViewSet, ListModelMixin):
     permission_classes = (AllowAny,)
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
