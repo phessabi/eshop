35c6cef604ea09cbf3d05704f58a7c688c8e98ec
Hossein Keshavarz <hoseinkeshavarz1997@gmail.com>
2020-02-02 21:46:49 +0330

    update allowed hosts again...

--- app/app/settings.py
+++ app/app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '3w!0h3#&6hwc96f$m2l3+4a_vp9y)odh15if=x4v6f3_+(ux1e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['194.5.192.129']
+ALLOWED_HOSTS = ['194.5.192.129', '127.0.0.1']
 
 
 # Application definition
