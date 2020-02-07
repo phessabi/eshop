7b53329bfa756d554cc349ed766475f755df96d1
Hossein Keshavarz <hoseinkeshavarz1997@gmail.com>
2020-02-08 00:13:08 +0330

    fuck

--- app/purchase/urls.py
+++ app/purchase/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from purchase.views import CartViewSet
+from purchase.views import CartViewSet, AddCartViewSet
 
 router = routers.DefaultRouter()
 router.register('cart', CartViewSet)
