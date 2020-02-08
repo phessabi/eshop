5882e74fe3e6eb362cb15d515bcb031ef336b888
Hossein Keshavarz <hoseinkeshavarz1997@gmail.com>
2020-02-08 20:26:57 +0330

    bring back stupid add to cart post api

--- app/purchase/urls.py
+++ app/purchase/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from purchase.views import CartViewSet, OrderViewSet
+from purchase.views import CartViewSet, OrderViewSet, AddToCartView
 
 router = routers.DefaultRouter()
 router.register('cart', CartViewSet)
+router.register('add-cart', AddToCartView)
 router.register('order', OrderViewSet)
 
 urlpatterns = [
--- app/purchase/views/__init__.py
+++ app/purchase/views/__init__.py
@@ -1,2 +1,3 @@
 from .cart_views.cart import CartViewSet
+from .cart_views.cart import AddToCartView
 from .order_views.order import OrderViewSet
