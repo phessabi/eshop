3873f60c465af384e25369b445720b045425102a
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-02-16 22:29:51 +0330

    Add campaign model

---
+++ app/purchase/models/campaign.py
@@ -0,0 +1,29 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+from accounts.models import Vendor
+
+
+class Campaign(models.Model):
+    vendor = models.OneToOneField(
+        to=Vendor,
+        on_delete=models.CASCADE,
+        verbose_name='فروشنده'
+    )
+
+    start_datetime = models.DateTimeField(
+        verbose_name='زمان شروع'
+    )
+
+    end_datetime = models.DateTimeField(
+        verbose_name='زمان پایان'
+    )
+
+    sale_amount = models.FloatField(
+        validators=(MinValueValidator(0), MaxValueValidator(1)),
+        verbose_name='مقدار تخفیف'
+    )
+
+    class Meta:
+        verbose_name = 'کمپین'
+        verbose_name_plural = 'کمپین‌ها'
