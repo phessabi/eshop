3619c2115c5fbc290ca02b09644d1dc883b237c8
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-02-17 02:26:01 +0330

    Add campaign to admin

--- app/purchase/admin.py
+++ app/purchase/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from purchase.models import Cart, Payment, Order
+from purchase.models import Cart, Payment, Order, Campaign
 
 admin.site.register(Cart)
 admin.site.register(Order)
 admin.site.register(Payment)
+admin.site.register(Campaign)
