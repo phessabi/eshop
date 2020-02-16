d96fab3b25fe466cbcbb834f6e4aadfbb7eaf4d0
Hossein Keshavarz <hoseinkeshavarz1997@gmail.com>
2020-02-16 17:07:04 +0330

    enable image uploading in back-end

--- app/products/serializers/product.py
+++ app/products/serializers/product.py
@@ -6,16 +6,28 @@ from products.serializers.specification import SpecificationSerializer
 class ProductSerializer(serializers.ModelSerializer):
     specifications = SpecificationSerializer(many=True, read_only=True)
     category_name = serializers.SerializerMethodField()
+    vendor_name = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
-        fields = ('id', 'title', 'category', 'category_name', 'price', 'express', 'specifications', 'vendor', 'image')
+        fields = ('id', 'title', 'category', 'category_name', 'price', 'express', 'specifications', 'vendor', 'vendor_name')
         read_only_fields = ('id', 'specifications')
 
     @staticmethod
     def get_category_name(instance: Product):
         return instance.category.name
 
+    @staticmethod
+    def get_vendor_name(instance: Product):
+        return instance.vendor.name
+
     def create(self, validated_data):
         validated_data['vendor_id'] = self.context.get('vendor_id')
         return super().create(validated_data)
+
+
+class ImageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = 'image'
