6d9efaf07576987c200889f43b6d2a482f3ae117
Ruhollah Sekaleshfar <r.sekaleshfar@gmail.com>
2020-02-17 04:02:19 +0330

    Add vendor payment model

--- app/purchase/admin.py
+++ app/purchase/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from purchase.models import Cart, Payment, Order, Campaign
+from purchase.models import Cart, Payment, Order, Campaign, VendorPayment
 
 admin.site.register(Cart)
 admin.site.register(Order)
 admin.site.register(Payment)
 admin.site.register(Campaign)
+admin.site.register(VendorPayment)
--- app/purchase/models/__init__.py
+++ app/purchase/models/__init__.py
@@ -1,4 +1,5 @@
 from .cart import Cart
+from .vendor_payment import VendorPayment
 from .order import Order
 from .payment import Payment
 from .campaign import Campaign
