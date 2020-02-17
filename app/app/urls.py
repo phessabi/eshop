e82a18adf6738b29c6c237cb284bc93af0654f5a
Hossein Keshavarz <hoseinkeshavarz1997@gmail.com>
2020-02-17 10:48:16 +0330

    enable serving images

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+
+from app import settings
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -28,4 +31,5 @@ urlpatterns = [
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, })
 ]
