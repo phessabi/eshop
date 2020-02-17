ec3e5b2a24bf6c0fec10f71a24d87bf9a78369d4
Hossein Keshavarz <hoseinkeshavarz1997@gmail.com>
2020-02-17 14:55:20 +0330

    some prints for debug

--- app/purchase/views/campaign.py
+++ app/purchase/views/campaign.py
@@ -19,6 +19,11 @@ class CampaignViewSet(CreateModelMixin,
 
     def create(self, request, *args, **kwargs):
         data = request.data
+        print('***** campaign view *****')
+        print(data['start_datetime'])
+        print(type(data['start_datetime']))
+        print(data['end_datetime'])
+        print(type(data['end_datetime']))
         data['sale_amount'] = data['sale_amount'] / 100
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
